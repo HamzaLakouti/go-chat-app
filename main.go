22e88e4e7cf46a46d509ccff7625c36b78c7e2d1
Hamza Lakouti <hamza.lakouti@gmail.com>
2025-06-21 17:45:24 +0100

    add multi-user

--- main.go
+++ main.go
@@ -13,6 +13,51 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+// Represents a single ws connection
+type client struct {
+	conn *websocket.Conn
+	send chan []byte // channel of messages to send
+}
+
+// Manages all clients and broadcasts messages
+type hub struct {
+	clients    map[*client]bool
+    broadcast  chan []byte
+    register   chan *client
+    unregister chan *client
+}
+
+var chatHub = hub{
+    clients:    make(map[*client]bool),
+    broadcast:  make(chan []byte),
+    register:   make(chan *client),
+    unregister: make(chan *client),
+}
+
+// Start the hub
+func (h *hub) run() {
+    for {
+        select {
+        case c := <-h.register:
+            h.clients[c] = true
+        case c := <-h.unregister:
+            if _, ok := h.clients[c]; ok {
+                delete(h.clients, c)
+                close(c.send)
+            }
+        case msg := <-h.broadcast:
+            for c := range h.clients {
+                select {
+                case c.send <- msg:
+                default:
+                    close(c.send)
+                    delete(h.clients, c)
+                }
+            }
+        }
+    }
+}
+
 // Home page handler
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Chat App!")
@@ -30,27 +75,48 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Upgrade error: ", err)
 		return
 	}
-	defer conn.Close()
-
-	for {
-		// Read message from browser
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("Read error: ", err)
-			break
-		}
-		fmt.Printf("Recieved: %s\n", msg)
-
-		// Echo message back
-		err = conn.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			fmt.Println("Write error: ", err)
-			break
-		}
+	
+	c := &client{
+		conn: conn,
+		send: make(chan []byte),
 	}
+
+	chatHub.register <- c
+
+    go c.writePump()
+    c.readPump()
+}
+
+// Read messages from this client and broadcast
+func (c *client) readPump() {
+    defer func() {
+        chatHub.unregister <- c
+        c.conn.Close()
+    }()
+
+    for {
+        _, msg, err := c.conn.ReadMessage()
+        if err != nil {
+            break
+        }
+        chatHub.broadcast <- msg
+    }
+}
+
+// Write messages to this client
+func (c *client) writePump() {
+    defer c.conn.Close()
+    for msg := range c.send {
+        err := c.conn.WriteMessage(websocket.TextMessage, msg)
+        if err != nil {
+            break
+        }
+    }
 }
 
 func main() {
+	go chatHub.run()
+	
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/chat", chatHandler)
 	http.HandleFunc("/ws", wsHandler)
